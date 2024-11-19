7704d96e64d90dd10fd74e44e3e41aafa21e48ee
Jailson Zarur <jailsoncardoz123@gmail.com>
2024-11-19 13:13:49 -0300

    Definição do modelo de Usuário e também da estratégia de modelagem dos dados no banco de Dados(Single Table Inheritance).

---
+++ backend/src/infra/orm/models/models.py
@@ -0,0 +1,19 @@
+from src.infra.orm.config.database import Base
+from sqlalchemy.orm import mapped_column, Mapped
+
+# Usuário
+class User( Base ):
+    __tablename__ = "users"
+
+    #CPF e EMAIL são únicos por Usuário
+    id: Mapped[str] = mapped_column(primary_key=True)
+    cpf_cnpj: Mapped[str] = mapped_column(nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(nullable=False)
+    type: Mapped[str] = mapped_column(nullable=False)
+
+# Note que, um usuário e lojista serão registrados na mesma tabela 
+# de banco de dados(Single Table Inheritance). Como a única de diferença 
+# entre eles será o método de tranferência, 
+# é mais ágil(Tanto em implementação como em consulta) adotar esse método,
+
